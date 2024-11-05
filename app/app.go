abed1828ce7aac5e97e7bf95bd94a6566d981d55
engmtcdrm <deedsoftheforsaken@gmail.com>
2024-11-04 20:07:29 -0500

    Initial commit

---
+++ app/app.go
@@ -0,0 +1,14 @@
+package app
+
+const (
+	// Application Name
+	Name = "crash-and-burn"
+
+	// Application Description
+	Description = "A simple utility for randomly generating success and failure return codes"
+)
+
+var (
+	// Application Version
+	Version = "dev"
+)
