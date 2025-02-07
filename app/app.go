fdbfb7f1e0070f6fcb1221b70b387bdfc249deba
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-02-07 18:24:24 -0500

    Refactored getSemVersion into app package and renamed to SemVersion

--- app/app.go
+++ app/app.go
@@ -1,5 +1,7 @@
 package app
 
+import "regexp"
+
 const (
 	// Application Name
 	Name = "crash-and-burn"
@@ -12,3 +14,21 @@ var (
 	// Application Version
 	Version = "dev"
 )
+
+// SemVersion returns the semantic version of the application version
+// If the version is not in semantic versioning format, it returns the
+// original value
+func SemVersion() string {
+	// Define the regular expression for semantic versioning
+	re := regexp.MustCompile(`^v?(\d+\.\d+\.\d+)$`)
+
+	match := re.FindStringSubmatch(Version)
+
+	// If there's a match return the semantic version
+	if len(match) > 1 {
+		return match[1]
+	}
+
+	// If no match, return the original input
+	return Version
+}
